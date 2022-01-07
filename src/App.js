fc8547b4dfb83c8834a1b3ff5d6b59e3a2235215
Ben Duncan <duncan.ben@gmail.com>
2022-01-07 16:18:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Editor from "./Editor";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Editor />
+    </div>
+  );
+}
